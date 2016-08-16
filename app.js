45c03e9895e89a5201f26dd35387df3e55bdd83d
gabrielkhofri <izotope115@gmail.com>
2016-08-16 13:37:59 -0700

    creating error status

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ function compare() {
     var input2 = document.getElementById('input2').value.toUpperCase();
 
     if (input1 && input2 != '') {
-
         history.push(input1, ' | ', input2);
-
         $('#history').append('<br>', history);
 
-        console.log(history); //TODO remove
+            if (input1 != input2) {
+               $('.message').append('<p class="alert alert-danger"><strong>ERR</strong> Hash Keys <strong>DO NOT MATCH!</strong></p>');
 
-        if (input1 != input2) {
+            }
 
-            $('.message').append(' ERR hash keys DO NOT MATCH!').addClass("alert alert-danger");
-        }
-        else {
-            $('.message').append(' hash is identical').removeClass("alert alert-danger").addClass("alert alert-success");
-        }
+            else if (input1 === input2) {
+               $('.message').append('<p class="alert alert-success">Hash is Identical</p>');
+            }
+    }
+
+    else {
+        $('.message').append('<p class="alert alert-warning">Please Enter Your Has Keys</p>');
     }
 
 }
